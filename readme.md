7017c907f0b02715f13354aa0fd072779cc5e254
cluster777 <davidlaksmanasan@gmail.com>
2021-07-23 16:57:27 +0700

    uhm kutata ulang supaya gak 1 folder

---
+++ readme.md
@@ -0,0 +1,17 @@
+__**Folder Kode**__
+pada folder ini berisi kode yeah semua kode yang ku buat sendiri atau ambil dari internet
+
+__**Folder kode rancangan atau pseudocode**__
+Pada folder ini saya menaruh rancangan algoritma yang kutangkap dari paper ataupun internet
+
+__**Folder Paper dan dokumentasi**__
+Pada folder ini berisi dokumentasi dan semua paper mengenai tugas akhir ini
+
+__**Folder Sharing dll**__
+yup sisa dari topik khusus
+
+Folder testcase dan generator {bold underline}
+folder berisi testcase generator buatan sendiri dan test case yang dihasilkan
+
+__**Folder Referensi**__
+folder berisi semua referensi yang saya gunakan yang dari [HP](https://www.hpl.hp.com/techreports/2012/HPL-2012-40R1.pdf) merupakan referensi utama
--- testcase dan generator/testcase generator.cpp
+++ testcase dan generator/testcase generator.cpp
@@ -27,18 +27,23 @@ int main(){
 	string plist[6]={"rook","bishop","pawn","king","queen","knight"};
 	srand(time(0));
 	ofstream outfile;
+	
+	//make 2 output file change the max i to make less or more
 	for(int i=1;i<3;i++){
 		fname="in"+itos(i)+".txt";
 		outfile.open(fname.c_str());
+		//the variable l for location p for piece max 64 min 1
 		int n=10,l=60,p=60;
 		outfile<<n<<endl;
 		
 		for(int j=0;j<n;j++){
 			outfile<<l<<" "<<p<<endl;
 			for(int k=0;k<l;k++){
+				//the position and name of piece if want only certain piece in board change random choose
 				outfile<<randomint(8,1)<<" "<<randomint(8,1)<<" "<<randomchoose(plist)<<endl;
 			}
 			for(int k=0;k<l;k++){
+				//the target location on board trully random trust me
 				outfile<<randomint(8,1)<<" "<<randomint(8,1)<<endl;
 			}
 		}
